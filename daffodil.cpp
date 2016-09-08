c59021ca8807c2d7b455896a187fb9cc75d58dee
Chen Su <420703660@qq.com>
2016-09-08 11:13:07 +0800

    2.1

---
+++ daffodil.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+    for(int i = 100; i <= 999; i++){
+        int a = i/100;
+        int b = (i%100)/10;
+        int c = (i%10);
+        if(a*a*a + b*b*b + c*c*c == i)
+            cout << i << '\t';
+    }
+    cout << endl;
+    return 0;
+}
